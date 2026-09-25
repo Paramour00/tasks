ea06696114ef1b2882b44a6aa2bdd959e83833bf
Paramour00 <shongkey95@gmail.com>
2026-09-25 04:43:12 +0000

    task1

---
+++ task1/HelloWorldAppldApp.java
@@ -0,0 +1,13 @@
+//package task1;
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+ class HelloWorldAppldApp{
+
+    public static void main(String[] args) {
+        System.out.println("KAMUSTA MUNDO!");
+    }
+}
+
+
